ed7a82e1aad63a1e07fa11769bb4d65556190792
Екатерина Лукинова <katyushta@rambler.ru>
2020-08-22 19:22:21 +0300

    pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    AOS.init({
+        duration: 1300,
+    });
+
+    new WOW().init();
+
     $('.button__dropdown').click(function(){
         $('.link-box').toggleClass('show')
     });
@@ -8,40 +14,104 @@ $(document).ready(function () {
         $('.link-box').removeClass('show')
     })
 
-    //main screen carousel
-
-    var current_pos;
-
-    $('.main-carousel').owlCarousel({
-        items: 1,
-        mouseDrag: false,
-        touchDrag: false,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        nav: true,
-        navText: ['', ''],
-        dots: true,
-        onInitialized: function(e) {
-            current_pos = e.item.index;
-
-        },
-        onTranslate: function(e) {
-            var direction = e.item.index > current_pos? 1 : 0,
-                indicator_c = direction? 'next' : 'prev',
-                $dots = $(e.currentTarget).find('.owl-dots'),
-                $current_dot = $dots.children().eq(current_pos);
-
-            $current_dot.html('<div class="dot-indicator '+ indicator_c +'">');
-
-            current_pos = e.item.index;
-
-            setTimeout(function() {
-                $current_dot.html('');
-            }, 200);
-        }
+    $("nav ul li").click(function() {
+        $("li").removeClass("active");
+        $(this).addClass("active");
     });
 
+
+    var currentSlide = 0;
+    var currentSection = 0;
+    $('#fullpage').fullpage({
+        sectionSelector: '.vertical-scrolling',
+        slideSelector: '.horizontal-scrolling',
+        navigation: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'top',
+        controlArrows: false,
+        anchors: ['section1', 'section2', 'section3'],
+        menu: '#menu',
+
+        afterRender: function(){
+            currentSection = getSection();
+            currentSlide = getSlide();
+        }
+
+    });
+
+    // var scrolledPixels = 0;
+    $('html').bind('wheel', function (e) {
+        // scrolledPixels += Math.abs(e.originalEvent.deltaY);
+
+        // if (scrolledPixels >= 500) {
+            scrolledPixels = 0;
+            // scroll direction
+            if (e.originalEvent.deltaY < 0) {
+                // is previous slide
+                if ($('.fp-section.active').find('.fp-slide.active').prev('.fp-slide').length) {
+                    $.fn.fullpage.moveTo('section' + currentSection, currentSlide - 1);
+                    currentSlide--;
+                } else {
+                    currentSlide = $('#page' + currentSection - 1).find('.fp-slide').length - 1;
+                    if (currentSection > 1) {
+                        currentSection--;
+                        $.fn.fullpage.moveTo('section' + (currentSection + 1), currentSlide);
+                    }
+                }
+            }
+            else if (e.originalEvent.deltaY > 0) {
+                // is next slide
+                if ($('.fp-section.active').find('.fp-slide.active').next('.fp-slide').length) {
+                    $.fn.fullpage.moveTo('section' + currentSection, currentSlide + 1);
+                    currentSlide++;
+                } else {
+                    if (currentSection < 2) {
+                        currentSection++;
+                    }
+                    $.fn.fullpage.moveTo('section' + (currentSection), 0);
+                    currentSlide = 0;
+                }
+            }
+        // }
+    });
+
+    $('#menu1').click(function(){
+        currentSlide = 0;
+        currentSection = 1;
+    });
+
+    $('#menu2').click(function(){
+        currentSlide = 0;
+        currentSection = 2;
+    });
+
+    $('#menu3').click(function(){
+        currentSlide = 0;
+        currentSection = 3;
+    });
+
+    $('#menu4').click(function(){
+        currentSlide = 0;
+        currentSection = 4;
+    });
+
+    //tabs
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this).addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    })
+
+    function getSection() {
+        return parseInt($('.fp-section.active').first().attr('id').replace(/page/g,''));
+    }
+
+    function getSlide() {
+        return $('.fp-section.active').find('.fp-slide.active').index();
+    }
 });
+
+
 
 
 
